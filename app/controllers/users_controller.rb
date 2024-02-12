be413b29842acbf4f81bcc72a0857c3530faa7cc
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-02-12 12:00:18 +0900

    feat: ユーザー情報（名前・プロフィール・アイコン）編集機能追加、ユーザーページに登録内容を表示 #1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:profile_edit, :profile_update]
+
   def show
+    @user = current_user
   end
 
   def profile_edit
+    @user = current_user
   end
 
   def profile_update
+    @user = current_user
+    if @user.update(profile_params)
+      redirect_to users_show_path
+    else
+      render 'profile_edit'
+    end
+  end
+
+  def profile_params
+    params.require(:user).permit(:name, :profile, :icon)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :icon
+
   validates :name, presence: true
   validates :profile, length: { maximum: 300 }
 end

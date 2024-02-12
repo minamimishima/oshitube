f71ff4f15babf96ee388f5c750757b6d2530f0aa
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-02-12 09:47:55 +0900

    feat: プロフィールページ表示・編集のためUsersコントローラを作成 #1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def profile_edit
+  end
+
+  def profile_update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
+  get "users/show", to: "users#show"
+  get "profile/edit", to: "users#profile_edit"
+  post "profile/edit", to: "users#profile_update"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

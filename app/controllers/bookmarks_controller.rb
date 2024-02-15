cd6f84cad6ea57e5ccc598caf4bd66f7153e30dd
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-02-15 13:13:01 +0900

    feat: ブックマーク登録機能実装、is_publicのデフォルト値を設定したためバリデーションの設定を削除 #3

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,4 +4,28 @@ class BookmarksController < ApplicationController
   def index
     @bookmarks = Bookmark.all
   end
+
+  def new
+    @bookmark = Bookmark.new
+    @user = current_user
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @user = current_user
+    if @bookmark.save
+      flash[:notice] = "登録完了しました"
+      redirect_to bookmarks_path
+    else
+      flash[:notice] = @bookmark.errors.full_messages.join(", ")
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    
+  def bookmark_params
+    params.require(:bookmark).permit(:user_id, :url, :description, :is_public)
+  end
+
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,9 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
-  
-  with_options presence: true do
-    validates :url
-    validates :is_public
-  end
+
+  validates :url, presence: true
   validates :description, length: { maximum: 300 }
 end

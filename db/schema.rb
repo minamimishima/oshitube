a24b4d7ac33576261bf5d42db30e3a4c919738a8
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-02-18 00:01:16 +0900

    feat: 動画の表示・検索に使用するためbookmarksテーブルにvideo_idカラムを追加 #3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_16_040305) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_17_145908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_16_040305) do
     t.boolean "is_public", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video_id"
   end
 
   create_table "users", force: :cascade do |t|

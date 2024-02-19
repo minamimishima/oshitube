9a9e5ba305e24e339c89443cac4b255e1d260264
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-02-19 23:11:14 +0900

    refactor: viewファイルの記述を見やすくするためヘルパーをリファクタリング #4

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,11 +1,21 @@
 module BookmarksHelper
-  def youtube_data(video_id)
+  def video_attribute(video_id, attribute)
     require 'google/apis/youtube_v3'
 
     youtube = Google::Apis::YoutubeV3::YouTubeService.new
     youtube.key = ENV['YOUTUBE_API_KEY']
-    Rails.cache.fetch("bookmark_#{video_id}", expires_in: 12.hours) do
+    bookmark_video = Rails.cache.fetch("bookmark_#{video_id}", expires_in: 12.hours) do
       youtube.list_videos('snippet', id: video_id)
     end.items.first
+    bookmark_video.snippet.send(attribute)
   end
+
+  def video_title(video_id)
+    video_attribute(video_id, :title)
+  end
+
+  def video_description(video_id)
+    video_attribute(video_id, :description)
+  end
+
 end

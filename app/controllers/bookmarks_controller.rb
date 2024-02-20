ba5576189c666459ee4dcabfea7945bed0491a6d
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-02-20 23:46:51 +0900

    fix: ブックマークのURLを変更した際、video_idも変更されるようupdateメソッドを修正・VIDEO_ID_PATTERNを定数として定義 #3

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,7 @@
 class BookmarksController < ApplicationController
   before_action :authenticate_user!
   before_action :get_current_user
+  VIDEO_ID_PATTERN =  /(?:https:\/\/www\.youtube\.com(?:\/embed\/|\/watch\?v=)|https:\/\/youtu\.be\/|https:\/\/m\.youtube\.com\/watch\?v=)([\w-]{11})/
 
   def index
     @bookmarks = Bookmark.all.order(created_at: :desc).page(params[:page])
@@ -12,8 +13,7 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    video_id_pattern = /(?:https:\/\/www\.youtube\.com(?:\/embed\/|\/watch\?v=)|https:\/\/youtu\.be\/|https:\/\/m\.youtube\.com\/watch\?v=)([\w-]{11})/
-    @bookmark.video_id = @bookmark.url.match(video_id_pattern)[1]
+    @bookmark.video_id = @bookmark.url.match(VIDEO_ID_PATTERN)[1]
     if @bookmark.save
       flash[:notice] = "登録完了しました"
       redirect_to bookmarks_path
@@ -33,6 +33,7 @@ class BookmarksController < ApplicationController
 
   def update
     @bookmark = Bookmark.find(params[:id])
+    @bookmark.video_id = params[:bookmark][:url].match(VIDEO_ID_PATTERN)[1]
     if @bookmark.update(bookmark_params)
       flash[:notice] = "編集完了しました"
       redirect_to bookmark_path(@bookmark.id)
@@ -57,5 +58,9 @@ class BookmarksController < ApplicationController
 
   def bookmark_params
     params.require(:bookmark).permit(:user_id, :url, :description, :is_public, :video_id)
+  end
+
+  def video_id_pattern
+    /(?:https:\/\/www\.youtube\.com(?:\/embed\/|\/watch\?v=)|https:\/\/youtu\.be\/|https:\/\/m\.youtube\.com\/watch\?v=)([\w-]{11})/
   end
 end

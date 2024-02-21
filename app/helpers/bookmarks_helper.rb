e65b0ad47ffd8a9a3e2232fdde7f0597c27ad156
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-02-21 13:09:06 +0900

    refactor: ヘルパーメソッドをコントローラで使用しやすい形にしてapplication_controllerにinclude #4

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,25 +1,24 @@
 module BookmarksHelper
-  def video_attribute(video_id, attribute)
+  def get_video_data(video_id)
     require 'google/apis/youtube_v3'
 
     youtube = Google::Apis::YoutubeV3::YouTubeService.new
     youtube.key = ENV['YOUTUBE_API_KEY']
-    bookmark_video = Rails.cache.fetch("bookmark_#{video_id}", expires_in: 1.days) do
+    Rails.cache.fetch("bookmark_#{video_id}", expires_in: 1.days) do
       youtube.list_videos('snippet', id: video_id)
     end.items.first
-    bookmark_video.snippet.send(attribute)
   end
 
   def video_title(video_id)
-    video_attribute(video_id, :title)
+    get_video_data(video_id).snippet.title
   end
 
   def video_description(video_id)
-    video_attribute(video_id, :description)
+    get_video_data(video_id).snippet.description
   end
 
   def video_thumbnail(video_id)
-    thumbnails = video_attribute(video_id, :thumbnails)
+    thumbnails = get_video_data(video_id).snippet.thumbnails
     thumbnails_resolutions = [:maxres, :high, :standard, :medium, :default]
     thumbnails_resolutions.each do |resolution|
       return thumbnails.send(resolution).url if thumbnails.send(resolution)

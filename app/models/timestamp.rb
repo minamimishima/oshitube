477a29c82d301d28fe37ae4733eeee6a453e2307
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-02-27 23:39:25 +0900

    feat: 1件のブックマークに対してタイムスタンプは10件までしか登録できないようバリデーションを設定 #5

--- app/models/timestamp.rb
+++ app/models/timestamp.rb
@@ -1,3 +1,11 @@
 class Timestamp < ApplicationRecord
   belongs_to :bookmark
+
+  validate :bookmark_has_ten_or_less_timestamps
+
+  def bookmark_has_ten_or_less_timestamps
+    if bookmark.timestamps.count >= 10
+      errors.add(:base, "登録できるタイムスタンプは10件までです")
+    end
+  end
 end

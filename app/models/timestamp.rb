70fedfd21c6e2ed5f4d58353ad69cbe7b293755b
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-02-27 23:56:50 +0900

    feat: タイムスタンプの時間・分・秒は0以上の数字しか使用できないようバリデーションを設定 #5

--- app/models/timestamp.rb
+++ app/models/timestamp.rb
@@ -1,6 +1,11 @@
 class Timestamp < ApplicationRecord
   belongs_to :bookmark
 
+  with_options numericality: { greater_than_or_equal_to: 0 } do
+    validates :hour
+    validates :minute
+    validates :second
+  end
   validate :bookmark_has_ten_or_less_timestamps
 
   def bookmark_has_ten_or_less_timestamps

270fdd30c4c77050a8b48ed0c4fc04cf42a9965d
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-02 23:01:54 +0900

    feat: 各タイムスタンプに対応するクリックイベントの実装 #5

--- app/javascript/bookmarks/timestamps.js
+++ app/javascript/bookmarks/timestamps.js
@@ -1,5 +1,59 @@
-let timestamp = document.getElementById('timestamp');
-timestamp.addEventListener('click', function(){
+let timestamp0 = document.getElementById('timestamp-0');
+timestamp0.addEventListener('click', function() {
   let video = document.getElementById('bookmark-video');
-  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=60" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
-})
+  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[0]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+});
+
+let timestamp1 = document.getElementById('timestamp-1');
+timestamp1.addEventListener('click', function() {
+  let video = document.getElementById('bookmark-video');
+  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[1]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+});
+
+let timestamp2 = document.getElementById('timestamp-2');
+timestamp2.addEventListener('click', function() {
+  let video = document.getElementById('bookmark-video');
+  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[2]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+});
+
+let timestamp3 = document.getElementById('timestamp-3');
+timestamp3.addEventListener('click', function() {
+  let video = document.getElementById('bookmark-video');
+  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[3]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+});
+
+let timestamp4 = document.getElementById('timestamp-4');
+timestamp4.addEventListener('click', function() {
+  let video = document.getElementById('bookmark-video');
+  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[4]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+});
+
+let timestamp5 = document.getElementById('timestamp-5');
+timestamp5.addEventListener('click', function() {
+  let video = document.getElementById('bookmark-video');
+  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[5]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+});
+
+let timestamp6 = document.getElementById('timestamp-6');
+timestamp6.addEventListener('click', function() {
+  let video = document.getElementById('bookmark-video');
+  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[6]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+});
+
+let timestamp7 = document.getElementById('timestamp-7');
+timestamp7.addEventListener('click', function() {
+  let video = document.getElementById('bookmark-video');
+  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[7]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+});
+
+let timestamp8 = document.getElementById('timestamp-8');
+timestamp8.addEventListener('click', function() {
+  let video = document.getElementById('bookmark-video');
+  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[8]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+});
+
+let timestamp9 = document.getElementById('timestamp-9');
+timestamp9.addEventListener('click', function() {
+  let video = document.getElementById('bookmark-video');
+  video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[9]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+});

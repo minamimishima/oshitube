fa9a48da4442c173fec834a97f15d08234e8170e
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-05 22:13:23 +0900

    feat: categoryモデルを作成 #6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_225326) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_131103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_26_225326) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "video_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "category_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "timestamps", force: :cascade do |t|

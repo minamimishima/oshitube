e2816037f1aa6beede49cadac082a84d750c46f6
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-06 22:26:07 +0900

    feat: ログインしているユーザーが作成したカテゴリーのみ表示できるよう修正 #6

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,14 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :get_current_user
+
   def index
-    @categories = Category.all
+    @categories = @user.categories
+  end
+
+  private
+
+  def get_current_user
+    @user = current_user
   end
 end

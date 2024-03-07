88eb144d2bd01f38bb2403abe5bd41f2f6890866
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-07 21:08:30 +0900

    chore: 簡潔な表記にするため、category_nameからnameにカラム名を変更 #6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_131103) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_07_120339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_131103) do
 
   create_table "categories", force: :cascade do |t|
     t.integer "user_id"
-    t.string "category_name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

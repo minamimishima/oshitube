1d69a9c4937e0f36df8687cf15a38edff8730956
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-09 14:05:43 +0900

    feat: カテゴリー名が空で登録できないようバリデーションを設定 #6

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :category_bookmark, dependent: :destroy
   has_many :bookmarks, through: :category_bookmark
+
+  validates :name, presence: true
 end

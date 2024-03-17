b299c231a1dd5891a624a86fa264d5de74721736
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-17 21:14:59 +0900

    feat: 退会済みのユーザーが同じアカウントでログインできないよう、reject_deleted_userメソッドを作成しログイン時のbefore_actionとして設定 #18

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,23 @@
 class Users::SessionsController < Devise::SessionsController
+  before_action :reject_deleted_user, only: [:create]
+
   def guest_sign_in
     user = User.guest
     sign_in user
     redirect_to root_path
     flash[:notice] = "ゲストユーザーとしてログインしました"
+  end
+
+  def reject_deleted_user
+    @user = User.find_by(email: params[:user][:email])
+    if @user
+      if @user.is_deleted == true
+        flash[:notice] = "退会済みです。再度登録をしてご利用ください。"
+        redirect_to new_user_registration_path
+      end
+    else
+      flash[:notice] = "ユーザーが見つかりません"
+      redirect_to new_user_registration_path
+    end
   end
 end

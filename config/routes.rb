b0b3c859d35fd35585fa4d87f5ae512fdf118a1b
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-17 21:17:31 +0900

    feat: sessions_controller.rbを編集して使用するためルーティングを変更 #18

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end

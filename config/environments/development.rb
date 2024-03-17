f430c0c7ec6845794ed42302c583dbc99bf4fdf2
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-17 16:13:53 +0900

    refactor: 不要な記述を削除 #18

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,6 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   host = 'localhost:3000'
-  #Rails.application.routes.default_url_options[:host] = host
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {

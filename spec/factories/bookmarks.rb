ebd3ee68c351a8b93c4fc42082e690cda05f3b40
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-23 18:36:11 +0900

    style: rubocop-airbnbの指摘を受け、video_idの定義に関する記述を修正 #10

--- spec/factories/bookmarks.rb
+++ spec/factories/bookmarks.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :bookmark do
-    video_id = Faker::Alphanumeric.alphanumeric(number: 11)
+    video_id { Faker::Alphanumeric.alphanumeric(number: 11) }
     url { "https://www.youtube.com/watch?v=#{video_id}" }
-    video_id { video_id }
   end
 end

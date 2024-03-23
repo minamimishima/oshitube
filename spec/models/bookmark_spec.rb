b11238bc18488f366fddc637314dcd4ae32526de
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-23 20:02:45 +0900

    test: it内でテストデータを作成しているため、テストデータ名を簡潔なものに変更 #10

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -10,33 +10,33 @@ RSpec.describe Bookmark, type: :model do
     end
 
     it "URLがなければ無効であること" do
-      bookmark_with_no_url = FactoryBot.build(:bookmark, user_id: user.id, url: nil)
-      expect(bookmark_with_no_url).to be_invalid
+      bookmark = FactoryBot.build(:bookmark, user_id: user.id, url: nil)
+      expect(bookmark).to be_invalid
     end
 
     it "動画IDがなければ無効であること" do
-      bookmark_with_no_video_id = FactoryBot.build(:bookmark, user_id: user.id, video_id: nil)
-      expect(bookmark_with_no_video_id).to be_invalid
+      bookmark = FactoryBot.build(:bookmark, user_id: user.id, video_id: nil)
+      expect(bookmark).to be_invalid
     end
 
     it "動画IDが11文字より長ければ無効であること" do
-      bookmark_with_over_11_characters_video_id = FactoryBot.build(:bookmark, user_id: user.id, video_id: "ABCDEFGHIJKL")
-      expect(bookmark_with_over_11_characters_video_id).to be_invalid
+      bookmark = FactoryBot.build(:bookmark, user_id: user.id, video_id: "ABCDEFGHIJKL")
+      expect(bookmark).to be_invalid
     end
 
     it "動画IDが11文字より短ければ無効であること" do
-      bookmark_with_under_11_characters_video_id = FactoryBot.build(:bookmark, user_id: user.id, video_id: "ABCDEFGHIJ")
-      expect(bookmark_with_under_11_characters_video_id).to be_invalid
+      bookmark = FactoryBot.build(:bookmark, user_id: user.id, video_id: "ABCDEFGHIJ")
+      expect(bookmark).to be_invalid
     end
 
     it "動画のメモが300文字より長ければ無効であること" do
-      bookmark_with_over_300_characters_description = FactoryBot.build(:bookmark, user_id: user.id, description: "a" * 301)
-      expect(bookmark_with_over_300_characters_description).to be_invalid
+      bookmark = FactoryBot.build(:bookmark, user_id: user.id, description: "a" * 301)
+      expect(bookmark).to be_invalid
     end
 
     it "動画のメモが300文字以内であれば有効であること" do
-      bookmark_with_300_characters_description = FactoryBot.build(:bookmark, user_id: user.id, description: "a" * 300)
-      expect(bookmark_with_300_characters_description).to be_valid
+      bookmark = FactoryBot.build(:bookmark, user_id: user.id, description: "a" * 300)
+      expect(bookmark).to be_valid
     end
   end
 end

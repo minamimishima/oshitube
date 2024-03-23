8491ca87d6cf031c527ba0a95177941c0ccb4df6
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-23 22:51:13 +0900

    test: 記述を簡潔にするため、video_idが12文字・10文字の場合のトレイトを定義 #10

--- spec/factories/bookmarks.rb
+++ spec/factories/bookmarks.rb
@@ -4,4 +4,12 @@ FactoryBot.define do
     url { "https://www.youtube.com/watch?v=#{video_id}" }
     association :user
   end
+
+  trait :video_id_with_12_characters do
+    video_id { Faker::Alphanumeric.alphanumeric(number: 12) }
+  end
+
+  trait :video_id_with_10_characters do
+    video_id { Faker::Alphanumeric.alphanumeric(number: 10) }
+  end
 end

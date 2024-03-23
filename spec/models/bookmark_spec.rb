d5f943c2285a8675c6f2f63c57754704e03113f8
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-23 22:43:52 +0900

    test: associationを設定したためuser_idに関する記述を削除 #10

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -5,37 +5,37 @@ RSpec.describe Bookmark, type: :model do
 
   describe "バリデーションの検証" do
     it "URL・動画IDがあれば有効であること" do
-      bookmark = FactoryBot.build(:bookmark, user_id: user.id)
+      bookmark = FactoryBot.build(:bookmark)
       expect(bookmark).to be_valid
     end
 
     it "URLがなければ無効であること" do
-      bookmark = FactoryBot.build(:bookmark, user_id: user.id, url: nil)
+      bookmark = FactoryBot.build(:bookmark, url: nil)
       expect(bookmark).to be_invalid
     end
 
     it "動画IDがなければ無効であること" do
-      bookmark = FactoryBot.build(:bookmark, user_id: user.id, video_id: nil)
+      bookmark = FactoryBot.build(:bookmark, video_id: nil)
       expect(bookmark).to be_invalid
     end
 
     it "動画IDが11文字より長ければ無効であること" do
-      bookmark = FactoryBot.build(:bookmark, user_id: user.id, video_id: Faker::Alphanumeric.alphanumeric(number: 12))
+      bookmark = FactoryBot.build(:bookmark, video_id: Faker::Alphanumeric.alphanumeric(number: 12))
       expect(bookmark).to be_invalid
     end
 
     it "動画IDが11文字より短ければ無効であること" do
-      bookmark = FactoryBot.build(:bookmark, user_id: user.id, video_id: Faker::Alphanumeric.alphanumeric(number: 10))
+      bookmark = FactoryBot.build(:bookmark, video_id: Faker::Alphanumeric.alphanumeric(number: 10))
       expect(bookmark).to be_invalid
     end
 
     it "動画のメモが300文字より長ければ無効であること" do
-      bookmark = FactoryBot.build(:bookmark, user_id: user.id, description: "a" * 301)
+      bookmark = FactoryBot.build(:bookmark, description: "a" * 301)
       expect(bookmark).to be_invalid
     end
 
     it "動画のメモが300文字以内であれば有効であること" do
-      bookmark = FactoryBot.build(:bookmark, user_id: user.id, description: "a" * 300)
+      bookmark = FactoryBot.build(:bookmark, description: "a" * 300)
       expect(bookmark).to be_valid
     end
   end

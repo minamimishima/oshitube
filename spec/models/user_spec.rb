ea7b02b7e0f1647fd5c6de191f3ec16c9c85ebed
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-23 13:13:46 +0900

    fix: テストデータ名の変更・テスト内の記述の修正 #10

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { FactoryBot.build(:user) }
-  let(:user_no_name) { FactoryBot.build(:user, name: nil) }
-  let(:user_over_300_characters_profile) { FactoryBot.build(:user, profile: "a" * 301) }
-  let(:user_300_charactets_profile) { FactoryBot.build(:user, profile: "a" * 300) }
+  let(:user_with_no_name) { FactoryBot.build(:user, name: nil) }
+  let(:user_with_over_300_characters_profile) { FactoryBot.build(:user, profile: "a" * 301) }
+  let(:user_with_300_charactets_profile) { FactoryBot.build(:user, profile: "a" * 300) }
 
   describe "バリデーションの検証" do
     it "メールアドレス・パスワード・名前があれば有効であること" do
@@ -12,15 +12,15 @@ RSpec.describe User, type: :model do
     end
 
     it "名前がなければ無効であること" do
-      expect(user_no_name).to be_invalid
+      expect(user_with_no_name).to be_invalid
     end
 
     it "プロフィールが300文字より長ければ無効であること" do
-      expect(user_too_long_profile).to be_invalid
+      expect(user_with_over_300_characters_profile).to be_invalid
     end
 
     it "プロフィールが300文字以内であれば有効であること" do
-      expect(user_valid_profile).to be_valid
+      expect(user_with_300_charactets_profile).to be_valid
     end
   end
 end

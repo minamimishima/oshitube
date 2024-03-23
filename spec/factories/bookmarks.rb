9a36e016ef9821ff66b271018b9ba3ca695fc3e2
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-23 22:41:35 +0900

    test: 記述を簡潔にするためFactoryBotにassociationを設定 #10

--- spec/factories/bookmarks.rb
+++ spec/factories/bookmarks.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :bookmark do
     video_id { Faker::Alphanumeric.alphanumeric(number: 11) }
     url { "https://www.youtube.com/watch?v=#{video_id}" }
+    association :user
   end
 end

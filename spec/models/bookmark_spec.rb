f079e4988499049f23054e5dbcc8d66a8d6a998d
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-24 10:36:39 +0900

    test: テストを見やすくするため、contextを使用して有効な場合・無効な場合に分類 #10

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -4,39 +4,43 @@ RSpec.describe Bookmark, type: :model do
   let(:user) { FactoryBot.create(:user) }
 
   describe "バリデーションの検証" do
-    it "URL・動画IDがあれば有効であること" do
-      bookmark = FactoryBot.build(:bookmark)
-      expect(bookmark).to be_valid
+    context "有効な場合" do
+      it "URL・動画IDがあれば有効であること" do
+        bookmark = FactoryBot.build(:bookmark)
+        expect(bookmark).to be_valid
+      end
+
+      it "動画のメモが300文字以内であれば有効であること" do
+        bookmark = FactoryBot.build(:bookmark, description: "a" * 300)
+        expect(bookmark).to be_valid
+      end
     end
 
-    it "URLがなければ無効であること" do
-      bookmark = FactoryBot.build(:bookmark, url: nil)
-      expect(bookmark).to be_invalid
-    end
+    context "無効な場合" do
+      it "URLがなければ無効であること" do
+        bookmark = FactoryBot.build(:bookmark, url: nil)
+        expect(bookmark).to be_invalid
+      end
 
-    it "動画IDがなければ無効であること" do
-      bookmark = FactoryBot.build(:bookmark, video_id: nil)
-      expect(bookmark).to be_invalid
-    end
+      it "動画IDがなければ無効であること" do
+        bookmark = FactoryBot.build(:bookmark, video_id: nil)
+        expect(bookmark).to be_invalid
+      end
 
-    it "動画IDが11文字より長ければ無効であること" do
-      bookmark = FactoryBot.build(:bookmark, :video_id_with_12_characters)
-      expect(bookmark).to be_invalid
-    end
+      it "動画IDが11文字より長ければ無効であること" do
+        bookmark = FactoryBot.build(:bookmark, :video_id_with_12_characters)
+        expect(bookmark).to be_invalid
+      end
 
-    it "動画IDが11文字より短ければ無効であること" do
-      bookmark = FactoryBot.build(:bookmark, :video_id_with_10_characters)
-      expect(bookmark).to be_invalid
-    end
+      it "動画IDが11文字より短ければ無効であること" do
+        bookmark = FactoryBot.build(:bookmark, :video_id_with_10_characters)
+        expect(bookmark).to be_invalid
+      end
 
-    it "動画のメモが300文字より長ければ無効であること" do
-      bookmark = FactoryBot.build(:bookmark, description: "a" * 301)
-      expect(bookmark).to be_invalid
-    end
-
-    it "動画のメモが300文字以内であれば有効であること" do
-      bookmark = FactoryBot.build(:bookmark, description: "a" * 300)
-      expect(bookmark).to be_valid
+      it "動画のメモが300文字より長ければ無効であること" do
+        bookmark = FactoryBot.build(:bookmark, description: "a" * 301)
+        expect(bookmark).to be_invalid
+      end
     end
   end
 end

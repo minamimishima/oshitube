6aa8d73bc7f87ed7ca48aee481445525e83a690c
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-26 23:01:45 +0900

    refactor: 見やすくするためテストデータをit内で作成 #10

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
-  let(:user_with_no_name) { build(:user, name: nil) }
-  let(:user_with_over_300_characters_profile) { build(:user, profile: "a" * 301) }
-  let(:user_with_300_charactets_profile) { build(:user, profile: "a" * 300) }
 
   describe "バリデーションの検証" do
     it "メールアドレス・パスワード・名前があれば有効であること" do
@@ -12,15 +9,18 @@ RSpec.describe User, type: :model do
     end
 
     it "名前がなければ無効であること" do
-      expect(user_with_no_name).to be_invalid
+      user = build(:user, name: nil)
+      expect(user).to be_invalid
     end
 
     it "プロフィールが300文字より長ければ無効であること" do
-      expect(user_with_over_300_characters_profile).to be_invalid
+      user = build(:user, profile: "a" * 301)
+      expect(user).to be_invalid
     end
 
     it "プロフィールが300文字以内であれば有効であること" do
-      expect(user_with_300_charactets_profile).to be_valid
+      user = build(:user, profile: "a" * 300)
+      expect(user).to be_valid
     end
   end
 end

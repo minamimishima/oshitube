282d7abfb654553b58a6afbe2110f7729c1d01f9
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-26 19:22:26 +0900

    test: categoryモデルのバリデーションに関するテストを作成 #10

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーションの検証" do
+    it "カテゴリー名があれば有効であること" do
+      category = build(:category)
+      expect(category).to be_valid
+    end
+
+    it "カテゴリー名がない場合は無効であること" do
+      category = build(:category, name: nil)
+      expect(category).to be_invalid
+    end
+  end
 end

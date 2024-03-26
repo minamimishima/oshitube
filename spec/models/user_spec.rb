b81af72339f3807755ae9b57e02c04dc748787c3
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-26 23:20:04 +0900

    test: テストを見やすくするためcontextを使用して有効な場合・無効な場合に分類 #10

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,33 +4,37 @@ RSpec.describe User, type: :model do
   let(:user) { build(:user) }
 
   describe "バリデーションの検証" do
-    it "メールアドレス・パスワード・名前があれば有効であること" do
-      expect(user).to be_valid
+    context "有効な場合" do
+      it "メールアドレス・パスワード・名前があれば有効であること" do
+        expect(user).to be_valid
+      end
+
+      it "プロフィールが300文字以内であれば有効であること" do
+        user = build(:user, profile: "a" * 300)
+        expect(user).to be_valid
+      end
     end
 
-    it "メールアドレスがなければ無効であること" do
-      user = build(:user, email: nil)
-      expect(user).to be_invalid
-    end
+    context "無効な場合" do
+      it "メールアドレスがなければ無効であること" do
+        user = build(:user, email: nil)
+        expect(user).to be_invalid
+      end
 
-    it "パスワードがなければ無効であること" do
-      user = build(:user, password: nil)
-      expect(user).to be_invalid
-    end
+      it "パスワードがなければ無効であること" do
+        user = build(:user, password: nil)
+        expect(user).to be_invalid
+      end
 
-    it "名前がなければ無効であること" do
-      user = build(:user, name: nil)
-      expect(user).to be_invalid
-    end
+      it "名前がなければ無効であること" do
+        user = build(:user, name: nil)
+        expect(user).to be_invalid
+      end
 
-    it "プロフィールが300文字より長ければ無効であること" do
-      user = build(:user, profile: "a" * 301)
-      expect(user).to be_invalid
-    end
-
-    it "プロフィールが300文字以内であれば有効であること" do
-      user = build(:user, profile: "a" * 300)
-      expect(user).to be_valid
+      it "プロフィールが300文字より長ければ無効であること" do
+        user = build(:user, profile: "a" * 301)
+        expect(user).to be_invalid
+      end
     end
   end
 end

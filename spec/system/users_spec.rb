8c5cb87ec6002d91120bdefe8ce76d6599cd2dbc
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-03-30 23:15:50 +0900

    test: ユーザーの新規登録に関するテストを作成 #27

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   it "新規登録する" do
+    user = build(:user)
+    visit new_user_registration_path
+    fill_in "名前", with: user.name
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    fill_in "パスワード（確認）", with: user.password
+    click_button "登録"
+    expect(page).to have_selector ".notice", text: "アカウント登録が完了しました。"
   end
 
   it "退会する" do

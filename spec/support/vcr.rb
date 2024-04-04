f753be1dcd529bfc2a2a5f7259516310973261a1
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-04 22:08:42 +0900

    test: カセットがない場合に実際のHTTPリクエストをする必要はないため設定を削除 #27

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,6 +4,5 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr"
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.allow_http_connections_when_no_cassette = true
   config.filter_sensitive_data('<YOUTUBE_API_KEY>') { ENV['YOUTUBE_API_KEY'] }
 end

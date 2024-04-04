46c0ffd053365ab2990af3d84b8769d03569486e
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-04 20:09:01 +0900

    style: テスト内容の記述が冗長なため修正 #27

--- spec/helpers/bookmarks_helper_spec.rb
+++ spec/helpers/bookmarks_helper_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 RSpec.describe BookmarksHelper, type: :helper do
   describe "video_titleの検証" do
-    it "渡された動画IDを持つ動画が存在する場合に動画タイトルを返すこと" do
+    it "渡されたIDを持つ動画が存在する場合にタイトルを返すこと" do
     end
 
-    it "渡された動画IDを持つ動画が存在しない場合にVideo Not Foundを返すこと" do
+    it "渡されたIDを持つ動画が存在しない場合にVideo Not Foundを返すこと" do
     end
   end
 
   describe "video_descriptionの検証" do
-    it "渡された動画IDを持つ動画が存在する場合に概要欄を返すこと" do
+    it "渡されたIDを持つ動画が存在する場合に概要欄を返すこと" do
     end
 
-    it "渡された動画IDを持つ動画が存在しない場合にVideo Not Foundを返すこと" do
+    it "渡されたIDを持つ動画が存在しない場合にVideo Not Foundを返すこと" do
     end
   end
 
   describe "video_thumbnailの検証" do
-    it "渡された動画IDを持つ動画が存在する場合に、最も解像度の高いサムネイルのURLを返すこと" do
+    it "渡されたIDを持つ動画が存在する場合に、最も解像度の高いサムネイルのURLを返すこと" do
     end
 
-    it "渡された動画IDを持つ動画が存在しない場合にnot_found.pngを返すこと" do
+    it "渡されたIDを持つ動画が存在しない場合にnot_found.pngを返すこと" do
     end
   end
 end

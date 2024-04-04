92272e81fcf59237c6b98f23a6c4ab7223ef4cf7
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-04 20:07:06 +0900

    chore: 一度作成したカセットは上書きせずに使用し、methodとuriがリクエストと一致した場合にカセットを使用するよう設定を変更 #27

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,8 +5,4 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.allow_http_connections_when_no_cassette = true
-  config.default_cassette_options = {
-    record: :new_episodes,
-    match_requests_on: [:method, :uri, :body],
-  }
 end

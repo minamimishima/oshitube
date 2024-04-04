cc2f87cfd71033baa0075e945c1847935177afd4
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-05 08:31:48 +0900

    chore: カセット内で環境変数を使用できるよう、erb形式のデータを含めるオプションを追加 #27

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,5 +4,6 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr"
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.default_cassette_options = { :erb => true }
   config.filter_sensitive_data('<YOUTUBE_API_KEY>') { ENV['YOUTUBE_API_KEY'] }
 end

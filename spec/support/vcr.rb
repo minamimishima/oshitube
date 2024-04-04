786121301e2d43bf1e83711caca4ecb1d618edaa
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-05 08:44:07 +0900

    chore: selenium_chrome_headlessを使用するテストに影響が出るのを防ぐため、VCRを使用しないテストではHTTP通信を許可する設定に変更 #27

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,5 +5,6 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.default_cassette_options = { :erb => true }
+  config.allow_http_connections_when_no_cassette = true
   config.filter_sensitive_data('<YOUTUBE_API_KEY>') { ENV['YOUTUBE_API_KEY'] }
 end

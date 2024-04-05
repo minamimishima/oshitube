198b585bcf3eab98dc77e31459bdae67df351582
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-05 12:59:07 +0900

    test: テストデータに動画のメモを追加 #27

--- spec/factories/bookmarks.rb
+++ spec/factories/bookmarks.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :bookmark do
     video_id { Faker::Alphanumeric.alphanumeric(number: 11) }
     url { "https://www.youtube.com/watch?v=#{video_id}" }
+    description { Faker::JapaneseMedia::StudioGhibli.quote }
     association :user
   end
 end

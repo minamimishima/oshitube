a9fefb4c7ca5757ba135dd010ae5ae0b41a5b74d
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-05 09:27:35 +0900

    test: テストしたい項目を設定 #27

--- spec/system/bookmarks_spec.rb
+++ spec/system/bookmarks_spec.rb
@@ -1,9 +1,116 @@
 require 'rails_helper'
 
 RSpec.describe "Bookmarks", type: :system do
-  before do
-    driven_by(:rack_test)
+  context "ログインしている状態" do
+    context "ユーザー自身のデータに関する処理" do
+      it "ブックマーク作成ページが表示できること" do
+      end
+
+      it "ブックマークが作成できること" do
+      end
+
+      it "公開設定のブックマークが閲覧できること" do
+      end
+
+      it "非公開設定のブックマークが閲覧できること" do
+      end
+
+      it "編集ページへのリンクが表示されること" do
+      end
+
+      it "編集ページが表示できること" do
+      end
+
+      it "ブックマークを編集できること" do
+      end
+
+      it "削除リンクが表示されること" do
+      end
+
+      it "ブックマークを削除できること" do
+      end
+    end
+
+    context "自分以外のユーザーのデータに関する処理" do
+      it "公開設定のブックマークが閲覧できること" do
+      end
+
+      it "非公開設定のブックマークは閲覧できないこと" do
+      end
+
+      it "編集ページへのリンクが表示されないこと" do
+      end
+
+      it "編集ページは表示できないこと" do
+      end
+
+      it "削除リンクが表示されないこと" do
+      end
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context "ゲストユーザーとしてログインしている状態" do
+    context "ゲストユーザー自身のデータに関する処理" do
+      it "ブックマーク作成ページが表示できること" do
+      end
+
+      it "ブックマークが作成できること" do
+      end
+
+      it "公開設定のブックマークが閲覧できること" do
+      end
+
+      it "非公開設定のブックマークが閲覧できること" do
+      end
+
+      it "編集ページへのリンクが表示されること" do
+      end
+
+      it "編集ページが表示できること" do
+      end
+
+      it "ブックマークを編集できること" do
+      end
+
+      it "削除リンクが表示されること" do
+      end
+
+      it "ブックマークを削除できること" do
+      end
+    end
+
+    context "ゲストユーザー以外のユーザーのデータに関する処理" do
+      it "公開設定のブックマークが閲覧できること" do
+      end
+
+      it "非公開設定のブックマークは閲覧できないこと" do
+      end
+
+      it "編集ページへのリンクが表示されないこと" do
+      end
+
+      it "編集ページは表示できないこと" do
+      end
+
+      it "削除リンクが表示されないこと" do
+      end
+    end
+  end
+
+  context "ログインしていない状態" do
+    it "公開設定のブックマークが閲覧できること" do
+    end
+
+    it "非公開設定のブックマークは閲覧できないこと" do
+    end
+
+    it "編集ページへのリンクが表示されないこと" do
+    end
+
+    it "編集ページは表示できないこと" do
+    end
+
+    it "削除リンクが表示されないこと" do
+    end
+  end
 end

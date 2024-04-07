b4b7426b24f98585c07cde761dac6e82f60f9f9a
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-07 20:31:09 +0900

    test: テストしたい項目を作成 #27

--- spec/system/categories_spec.rb
+++ spec/system/categories_spec.rb
@@ -3,6 +3,32 @@ require 'rails_helper'
 RSpec.describe "Categories", type: :system do
   context "ログインしている状態" do
     context "ユーザー自身のデータに関する処理" do
+      it "カテゴリーを作成できること" do
+      end
+
+      it "カテゴリーを表示できること" do
+      end
+
+      it "カテゴリー名を編集できること" do
+      end
+
+      it "カテゴリーを削除できること" do
+      end
+
+      it "ブックマーク作成時にカテゴリーを作成できること" do
+      end
+
+      it "ブックマーク作成時にカテゴリーを登録できること" do
+      end
+
+      it "ブックマーク編集時にカテゴリーを作成できること" do
+      end
+
+      it "ブックマーク編集時にカテゴリーを登録できること" do
+      end
+
+      it "ブックマーク編集時にカテゴリーを登録解除できること" do
+      end
     end
 
     context "自分以外のユーザーのデータに関する処理" do

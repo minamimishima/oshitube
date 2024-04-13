8dae812409f981cbc7fb993744ad884e5821ceef
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-13 11:01:14 +0900

    test: contextを作成 #34

--- spec/requests/bookmarks_spec.rb
+++ spec/requests/bookmarks_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Bookmarks", type: :request do
-  describe "GET /bookmarks" do
-    it "works! (now write some real specs)" do
-      get bookmarks_path
-      expect(response).to have_http_status(200)
-    end
+  context "ログインしている状態" do
+  end
+
+  context "ゲストユーザーとしてログインしている状態" do
+  end
+
+  context "ログインしていない状態" do
   end
 end

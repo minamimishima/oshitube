9c9dbe78928dbdf5df5423a235a03c17b44f97ee
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-14 06:57:11 +0900

    test: contextを作成 #34

--- spec/requests/timestamps_spec.rb
+++ spec/requests/timestamps_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Timestamps", type: :request do
-  describe "GET /timestamps" do
-    it "works! (now write some real specs)" do
-      get timestamps_path
-      expect(response).to have_http_status(200)
-    end
+  context "ログインしている状態" do
+  end
+
+  context "ログインしていない状態" do
   end
 end

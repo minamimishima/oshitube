f5601aaabbbd9f3b9e968d8b26afae31fc189efb
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-13 11:38:04 +0900

    test: ログインしている状態で他のユーザーのブックマークを作成・編集・削除できないことを確認するテストを作成 #34

--- spec/requests/bookmarks_spec.rb
+++ spec/requests/bookmarks_spec.rb
@@ -2,6 +2,46 @@ require 'rails_helper'
 
 RSpec.describe "Bookmarks", type: :request do
   context "ログインしている状態" do
+    let(:user) { create(:user) }
+    let(:bookmark) { create(:bookmark) }
+    let(:other_user) { bookmark.user }
+
+    before do
+      sign_in user
+    end
+
+    it "自分以外のユーザーのデータとしてブックマークを作成できないこと" do
+      bookmark_params = {
+        :user_id => other_user.id,
+        :video_id => "abcdefghijk",
+        :url => "https://www.youtube.com/watch?v=abcdefghijk",
+      }
+      expect do
+        post bookmarks_path, params: { bookmark: bookmark_params }
+      end.to change { other_user.bookmarks.count }.by(0)
+    end
+
+    it "自分以外のユーザーのブックマークは編集できないこと" do
+      bookmark_params = {
+        :video_id => "abcdefghijk",
+        :url => "https://www.youtube.com/watch?v=abcdefghijk",
+        :description => "新しいメモ",
+        :is_public => true,
+      }
+      patch bookmark_path(bookmark), params: { bookmark: bookmark_params }
+      aggregate_failures do
+        expect(bookmark.reload.video_id).to_not eq "abcdefghijk"
+        expect(bookmark.reload.url).to_not eq "https://www.youtube.com/watch?v=abcdefghijk"
+        expect(bookmark.reload.description).to_not eq "abcdefghijk"
+        expect(bookmark.reload.is_public).to_not eq true
+      end
+    end
+
+    it "自分以外のユーザーのブックマークは削除できないこと" do
+      expect do
+        delete bookmark_path(bookmark)
+      end.to change { other_user.bookmarks.count }.by(0)
+    end
   end
 
   context "ゲストユーザーとしてログインしている状態" do

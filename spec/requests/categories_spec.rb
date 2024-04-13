9a4d994fc9cabd7e93a75794b8ec6c8e6ed4cf29
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-13 18:27:37 +0900

    test: ログインしていない状態で他のユーザーのカテゴリーを作成・編集・削除できないことを確認するテストを作成 #34

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -37,5 +37,32 @@ RSpec.describe "Categories", type: :request do
   end
 
   context "ログインしていない状態" do
+    let(:category) { create(:category) }
+    let(:user) { category.user }
+
+    it "他のユーザーのデータとしてカテゴリーを作成できないこと" do
+      category_params = {
+        :user_id => user.id,
+        :name => "新しいカテゴリー",
+      }
+      expect do
+        post categories_path, params: { category: category_params }
+      end.to change { user.categories.count }.by(0)
+    end
+
+    it "他のユーザーのカテゴリー名は編集できないこと" do
+      category_params = {
+        :user_id => user.id,
+        :name => "新しいカテゴリー名",
+      }
+      patch category_path(category), params: { category: category_params }
+      expect(category.reload.name).to_not eq "新しいカテゴリー名"
+    end
+
+    it "他のユーザーのカテゴリーを削除できないこと" do
+      expect do
+        delete category_path(category)
+      end.to change { user.categories.count }.by(0)
+    end
   end
 end

fbdb2c41867a2af6a99deeafa9e205c0cd71584d
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-13 18:06:18 +0900

    test: ログインしている状態で他のユーザーのカテゴリーを作成・編集・削除できないことを確認するテストを作成 #34

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,6 +2,38 @@ require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
   context "ログインしている状態" do
+    let(:user) { create(:user) }
+    let(:category) { create(:category) }
+    let(:other_user) { category.user }
+
+    before do
+      sign_in user
+    end
+
+    it "自分以外のユーザーのデータとしてカテゴリーを作成できないこと" do
+      category_params = {
+        :user_id => other_user.id,
+        :name => "新しいカテゴリー",
+      }
+      expect do
+        post categories_path, params: { category: category_params }
+      end.to change { other_user.categories.count }.by(0)
+    end
+
+    it "自分以外のユーザーのカテゴリー名は編集できないこと" do
+      category_params = {
+        :user_id => other_user.id,
+        :name => "新しいカテゴリー名",
+      }
+      patch category_path(category), params: { category: category_params }
+      expect(category.reload.name).to_not eq "新しいカテゴリー名"
+    end
+
+    it "自分以外のユーザーのカテゴリーを削除できないこと" do
+      expect do
+        delete category_path(category)
+      end.to change { other_user.categories.count }.by(0)
+    end
   end
 
   context "ログインしていない状態" do

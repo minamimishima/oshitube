7cf929b7329516fd9bc6e31af4ec445d764a5193
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-14 10:44:12 +0900

    test: ログインしていない状態で他のユーザーのタイムスタンプを作成・編集・削除できないことを確認するテストを作成 #34

--- spec/requests/timestamps_spec.rb
+++ spec/requests/timestamps_spec.rb
@@ -66,5 +66,61 @@ RSpec.describe "Timestamps", type: :request do
   end
 
   context "ログインしていない状態" do
+    let(:bookmark) { create(:bookmark) }
+    let(:timestamp) { create(:timestamp, bookmark: bookmark) }
+
+    it "他のユーザーのデータとしてタイムスタンプを作成できないこと" do
+      timestamp_params = {
+        bookmark_id: bookmark.id,
+        hour: 0,
+        minute: 0,
+        second: 0,
+        comment: "メモ",
+      }
+      expect do
+        post timestamps_path, params: { timestamp: timestamp_params }
+      end.to change { bookmark.timestamps.count }.by(0)
+    end
+
+    it "他のユーザーのタイムスタンプは編集できないこと" do
+      params = {
+        url: bookmark.url,
+        description: bookmark.description,
+        timestamps_attributes: {
+          "0": {
+            id: timestamp.id,
+            bookmark_id: bookmark.id,
+            hour: 0,
+            minute: 0,
+            second: 0,
+            comment: "コメント",
+            _destroy: 0,
+          },
+        },
+      }
+      patch bookmark_path(bookmark), params: { bookmark: params }
+      expect(timestamp.reload.hour).to_not eq 0
+    end
+
+    it "他のユーザーのタイムスタンプは削除できないこと" do
+      params = {
+        url: bookmark.url,
+        description: bookmark.description,
+        timestamps_attributes: {
+          "0": {
+            id: timestamp.id,
+            bookmark_id: bookmark.id,
+            hour: 0,
+            minute: 0,
+            second: 0,
+            comment: "コメント",
+            _destroy: 1,
+          },
+        },
+      }
+      expect do
+        patch bookmark_path(bookmark), params: { bookmark: params }
+      end.to change { bookmark.timestamps.count }.by(0)
+    end
   end
 end

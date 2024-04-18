603113ee6325916eff28e7b9b0d14f2b424f0a57
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-18 21:43:27 +0900

    refactor: bookmarksコントローラ内にはBookmarkクラスのインスタンスのみインスタンス変数としてセットするため、bookmarks/indexのビューファイル内で関連するデータを取得 #41

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,8 +5,6 @@ class BookmarksController < ApplicationController
 
   def index
     @bookmarks = @user.bookmarks.order(created_at: :desc).page(params[:page])
-    @categories = @user.categories
-    @category = Category.new
   end
 
   def new

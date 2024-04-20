d67f9479a8bc1f28f6c2fce3f8e39c6a0796ee65
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-20 17:21:09 +0900

    test: 有効なパスワードを8文字以上30文字以下に設定したため、テストデータの設定を変更 #42

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    password { Faker::Internet.password }
+    password { Faker::Internet.password(min_length: 8, max_length: 30) }
     name { Faker::Games::Pokemon.name }
     profile { Faker::Games::Pokemon.move }
   end

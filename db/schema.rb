84f2d4f3b31bd7951c93a69a639333996e9e4a09
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-04-20 09:00:58 +0900

    feat: 退会したユーザーが同じメールアドレスで再登録できるようにするため、メールアドレスのインデックスを削除 #42

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_17_080829) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_19_235215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,7 +90,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_17_080829) do
     t.string "name", null: false
     t.text "profile"
     t.boolean "is_deleted", default: false
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 

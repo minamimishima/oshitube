49a48be168e3199b09410709a1976e6370a8ee3b
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-05-09 22:54:58 +0900

    feat: カテゴリー作成フォームをturbo_frameで表示 #15

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   before_action :correct_user, only: [:show, :update, :destroy]
 
   def new
-    @category = Category.new(category_params)
+    @category = Category.new
   end
 
   def create
@@ -15,7 +15,7 @@ class CategoriesController < ApplicationController
       flash[:notice] = "カテゴリーを作成しました"
       redirect_to bookmarks_path
     else
-      render 'bookmarks/index', status: :unprocessable_entity
+      render 'new', status: :unprocessable_entity
     end
   end
 

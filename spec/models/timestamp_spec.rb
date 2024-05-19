9df76cad4e56a77547fa8caf9b6a35b62681f8dd
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-05-19 16:25:44 +0900

    test: 1つのブックマーク内でタイムスタンプが重複しないようuniquenessを設定したことに伴うテストの修正 #56

--- spec/models/timestamp_spec.rb
+++ spec/models/timestamp_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe Timestamp, type: :model do
   describe "バリデーションの検証" do
     it "既存のタイムスタンプが9件以下の場合正常に登録できること" do
       create_list(:timestamp, 9, bookmark: bookmark)
-      timestamp = build(:timestamp, bookmark_id: bookmark.id)
+      timestamp = build(:timestamp, hour: 1, minute: 1, second: 1, bookmark_id: bookmark.id)
       expect(timestamp).to be_valid
     end
 
     it "既存のタイムスタンプが10件存在する場合は登録できないこと" do
       create_list(:timestamp, 10, bookmark: bookmark)
-      timestamp = build(:timestamp, bookmark_id: bookmark.id)
+      timestamp = build(:timestamp, hour: 1, minute: 1, second: 1, bookmark_id: bookmark.id)
       expect(timestamp).to be_invalid
     end
   end

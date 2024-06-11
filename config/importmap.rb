5f3b095b4f1db1a56bbaeebe71b8512474834d63
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-06-11 13:46:49 +0900

    feat: パスワードの表示・非表示を切り替えるボタンを実装 #79

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "custom/bookmark_timestamps"
 pin "custom/home_timestamps"
+pin "custom/password_show"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true

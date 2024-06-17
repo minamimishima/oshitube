de339804db81a997566275cdacb37ba82a0c15f5
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-06-17 16:28:17 +0900

    refactor: customディレクトリ以下のJavaScriptファイルを纏めてピン留め #94

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,9 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "custom/bookmark_timestamps"
-pin "custom/home_timestamps"
-pin "custom/password_show"
-pin "custom/content_missing"
+pin_all_from "app/javascript/custom", under: "custom"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true

e65438b9205c1e8f7d0fe2a4753e2813f4b68d69
minamimishima <146907532+minamimishima@users.noreply.github.com>
2024-06-18 22:04:16 +0900

    refactor: アロー関数を使用した記述に変更 #94

--- app/javascript/custom/bookmark_timestamps.js
+++ app/javascript/custom/bookmark_timestamps.js
@@ -1,26 +1,26 @@
-function jumpToStartTime (i) {
-  let timestamp = document.getElementById(`timestamp-${i}`);
+const jumpToStartTime = (i) => {
+  const timestamp = document.getElementById(`timestamp-${i}`);
   if (timestamp) {
-    timestamp.addEventListener('click', function() {
-    let video = document.getElementById('bookmark-video');
+    timestamp.addEventListener('click', () => {
+    const video = document.getElementById('bookmark-video');
     video.innerHTML = `<iframe src="https://www.youtube.com/embed/${gon.video_id}?start=${gon.start_time_list[i]}&autoplay=1" class="youtube-video-player" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     });
   }
 }
 
-document.addEventListener('turbo:load', function() {
+document.addEventListener('turbo:load', () => {
   for (let i = 0; i <= gon.max_index; i++) {
     jumpToStartTime(i);
   }
 });
 
-document.addEventListener('turbo:render', function() {
+document.addEventListener('turbo:render', () => {
   for (let i = 0; i <= gon.max_index; i++) {
     jumpToStartTime(i);
   }
 });
 
-document.addEventListener('turbo:frame-render', function() {
+document.addEventListener('turbo:frame-render', () => {
   for (let i = 0; i <= gon.max_index; i++) {
     jumpToStartTime(i);
   }
